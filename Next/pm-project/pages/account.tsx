1866fd3e86ed7dc186110f6189fdf1e6b58bf4ce
therealgabryx <gabriele.concli@gmail.com>
2021-02-21 16:14:29 +0100

    added global styles and Google Auth flow

--- Next/pm-project/pages/account.tsx
+++ Next/pm-project/pages/account.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../lib/authContext'; 
+import { auth, googleAuthProvider, firestore } from '../lib/firebase'
 
 export default function account() {
+
+    const { user } = useContext(UserContext);
+
     return (
-        <div>
-            Account
+        <div className='main'>
+            <h1>Account</h1>
+            { user ? <SignOutButton /> : <SignInButton />}
         </div>
     )
-} 
+}
+
+// Sign in with Google Button 
+function SignInButton() {
+    const signInWithGoogle = async () => {
+        await auth.signInWithPopup(googleAuthProvider);
+    }
+
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+            <img src={'/google.png'} alt="Sign in with Google" />
+        Sign in with Google
+        </button>
+    )
+}
+
+// Sign out Button 
+function SignOutButton() {
+    return (
+        <button onClick={() => auth.signOut()}>
+            SignOut
+        </button>
+    )
+}
