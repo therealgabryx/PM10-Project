8799cdcf9dfe22dfd32e19b80e00aec5ce2d401b
therealgabryx <gabriele.concli@gmail.com>
2021-02-25 08:28:19 +0000

    Login ui changes

--- Next/pm-project/pages/account.tsx
+++ Next/pm-project/pages/account.tsx
@@ -2,21 +2,41 @@ import React, { useContext } from 'react'
 import { UserContext } from '../lib/authContext'; 
 import { auth, googleAuthProvider, firestore } from '../lib/firebase'
 
+import AccountStyles from '../styles/Account.module.css'
+
 export default function account() {
 
-    const { user } = useContext(UserContext);
+    // const { user } = useContext(UserContext);
+    const user = true;
 
     return (
         <div className='main'>
             { user ? <h1>Account</h1> : <h1>Login</h1> }
-            { user ? <SignOutButton /> : <SignInButton />}
+            { user ? <AccountInfo /> : <SignInForm />}
         </div>
     )
 }
 
 function SignInForm() {
-
+    return(
+        <div> 
+            <SignInButton />
+        </div>
+    )
 }
+
+function AccountInfo() {
+    return(
+        <div> 
+            <div className={AccountStyles.profileImage}></div> 
+            <div>
+                <p>Welcome back, <b>Gabryx</b>!</p> 
+            </div>
+            <SignOutButton />
+        </div>
+    )
+}
+
 
 // Sign in with Google Button 
 function SignInButton() {
