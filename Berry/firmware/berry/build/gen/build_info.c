94c9ee4e630df82e8f2cc8d8dc95e76f993c3170
Rossigg <56557442+Rossigg@users.noreply.github.com>
2021-04-29 15:52:06 +0200

    Directory fix
    Fixed Berry directory

--- Berry/firmware/berry/build/gen/build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *build_id = "20210428-133657";
-const char *build_timestamp = "2021-04-28T13:36:57Z";
-const char *build_version = "0.9";
--- Berry/firmware/berry/build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20210428-133654/2.19.1-gd52032f";
-const char *mg_build_timestamp = "2021-04-28T13:36:54Z";
-const char *mg_build_version = "2.19.1";
